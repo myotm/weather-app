a8c82cba2c10e7308217f6b442d93f4c4fadd2d8
myotm <myotm.18@gmail.com>
2022-07-21 16:24:39 +0800

    return weatherIcon and resultPage created

--- components/WeatherComponent.js
+++ components/WeatherComponent.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Link from 'next/link';
 
 export default function WeatherComponent() {
 
   const [zipCode, setZipCode] = React.useState("");
   const [countryCode, setCountryCode] = React.useState("");
-
+  const [iconCode, setIconCode] = React.useState("")
   
 
   const onZipCodeChange = (e) => {
@@ -14,30 +15,36 @@ export default function WeatherComponent() {
   const onCountryCodeChange = (e) => {
     const {value}  = e.target;
     setCountryCode(value);
-}
+  }
+ 
 
-  const callCoordinatesAPI = async () => {
+  const callWeatherAPI = async () => {
     try {
       const res = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode},${countryCode}&appid=3b7363a01be6f1a1d6dd6cc797d1107a`);
       const data = await res.json();
       res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=3b7363a01be6f1a1d6dd6cc797d1107a`)
       data = await res.json();
+      setIconCode(data.weather[0].icon);
       console.log(data);
     } catch (err) {
       console.log(err);
     }
   }
 
-  
-
 
   return (
     <div>
-        <div className="search">
-            <input type="text" value={zipCode} onChange={onZipCodeChange} />
-            <input type="text" value={countryCode} onChange={onCountryCodeChange}/>
+        <div>
+          <b>ZIP CODE</b>
+          <input type="text" value={zipCode} onChange={onZipCodeChange} />
         </div>
-        <button onClick={callCoordinatesAPI}>Get GeoCoordinates now!</button>
+        <div>
+          <b>COUNTRY CODE</b>
+          <input type="text" value={countryCode} onChange={onCountryCodeChange}/>
+        </div>
+        <Link href={`/resultPage`}>
+          <button onClick={callWeatherAPI}>Get Weather now!</button>
+        </Link>
     </div>
   )
 }
