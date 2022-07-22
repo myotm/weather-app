0d0fac72674b196cc489f85c74924af26aab941c
myotm <myotm.18@gmail.com>
2022-07-22 19:26:22 +0800

    fixed some build errors

--- components/WeatherComponent.js
+++ components/WeatherComponent.js
@@ -47,15 +47,15 @@ export default function WeatherComponent() {
           type="text" id="zipCode" name="zipCode" value={zipCode} onChange={onZipCodeChange} />
         </div>
         <div className='mb-4'>
-          <label className='block text-gray-700 text-sm font-bold mb-2' for='countryCode'>Country Code</label>
+          <label className='block text-gray-700 text-sm font-bold mb-2' >Country Code</label>
           <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
            type="text" id="countryCode" name="countryCode" value={countryCode} onChange={onCountryCodeChange} />
         </div>
           <div className='mb-4 flex flex-col'>
             <button className=' text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button'
             type="submit" >Submit</button>
-            <a class="font-bold text-xs text-blue-500 hover:text-blue-800" href="https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes">
-            Don't know your country code?
+            <a className="font-bold text-xs text-blue-500 hover:text-blue-800" href="https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes">
+            Do not know your country code?
             </a>
           </div>
           
